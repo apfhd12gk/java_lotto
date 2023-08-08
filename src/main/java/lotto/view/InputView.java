769981d2ed2147b8577b79fa424fe1baeb896d33
apfhd12gk <apfhd12gk@naver.com>
2023-08-08 21:07:48 +0900

    scanWinningNumber() 함수 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,7 +6,6 @@ import lotto.domain.Lottos;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputView {
@@ -17,8 +16,7 @@ public class InputView {
     private static final String MESSAGE_FOR_BONUS_LOTTO_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final String SPLIT_DELIMITER = ", ";
     private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
-    private static final Pattern LOTTO_PATTERN = Pattern.compile("(., )+.");
-    private static final String ERROR_MESSAGE_FOR_INVALID_INPUT_FORMAT = "6개의 숫자를 ', ' 로 구분해서 입력해주세요";
+
 
     public static int scanInputMoney(){
         System.out.println(MESSAGE_TO_GET_INPUT_MONEY);
@@ -36,20 +34,17 @@ public class InputView {
         System.out.print(System.lineSeparator());
         System.out.println(MESSAGE_FOR_WINNING_LOTTO_NUMBER);
         String userInput = SCANNER.nextLine();
-        validateInputFormat(userInput);
 
-        return Arrays.stream(userInput.split(SPLIT_DELIMITER))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-    }
-
-    private static void validateInputFormat(String userInput){
-        boolean matches =LOTTO_PATTERN.matcher(userInput).matches();
-
-        if(!matches){
-            throw new IllegalArgumentException(ERROR_MESSAGE_FOR_INVALID_INPUT_FORMAT);
+        try{
+            return Arrays.stream(userInput.split(SPLIT_DELIMITER))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("숫자만 입력해 주세요");
         }
     }
+
+
     public static int scanBonusNumber(){
         System.out.println(MESSAGE_FOR_BONUS_LOTTO_NUMBER);
         return Integer.parseInt(SCANNER.nextLine());
