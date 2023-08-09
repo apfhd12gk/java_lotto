dc8785df74eb0c4a6f02a8e4976a311f18a42b73
apfhd12gk <apfhd12gk@naver.com>
2023-08-09 21:03:17 +0900

    LottoNumbers 정적 상수로 가독성 올리기

--- src/main/java/lotto/constants/LottoNumbers.java
+++ src/main/java/lotto/constants/LottoNumbers.java
@@ -5,13 +5,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumbers {
+    public static final int LOTTO_NUMBER_START = 1;
+    public static final int LOTTO_NUMBER_END = 45;
     public static List<Integer> LOTTO_NUMBERS;
     static {
         List<Integer> lottoNumbers = new ArrayList<>();
-        for(int i = 1; i <= 45; i++){
+        for(int i = LOTTO_NUMBER_START; i <= LOTTO_NUMBER_END; i++){
             lottoNumbers.add(i);
         }
-        LOTTO_NUMBERS= Collections.unmodifiableList(lottoNumbers);
+        LOTTO_NUMBERS = Collections.unmodifiableList(lottoNumbers);
     }
     private LottoNumbers(){
 
