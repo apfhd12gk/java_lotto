68a3475fe62ce1f1b3788846f0e8d29ac1d1b272
apfhd12gk <apfhd12gk@naver.com>
2023-08-10 21:18:43 +0900

    parseInt에 대한 예외추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
+    public static final String MESSAGE_FOR_INVALID_NUMBER ="숫자만 입력해 주세요.";
     private static final String MESSAGE_TO_GET_INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String MESSAGE_FOR_LOTTO_COUNT = "%d개를 구매 했습니다.%n";
     private static final String MESSAGE_FOR_WINNING_LOTTO_NUMBER = "당첨 번호를 입력해 주세요.";
@@ -20,7 +21,12 @@ public class InputView {
 
     public static int scanInputMoney(){
         System.out.println(MESSAGE_TO_GET_INPUT_MONEY);
-        return Integer.parseInt(SCANNER.nextLine());
+        try{
+            return Integer.parseInt(SCANNER.nextLine());
+        } catch (NumberFormatException exception){
+            throw new IllegalArgumentException(MESSAGE_FOR_INVALID_NUMBER);
+        }
+
     }
 
     public static void printException(Exception exception){
@@ -47,7 +53,11 @@ public class InputView {
 
     public static int scanBonusNumber(){
         System.out.println(MESSAGE_FOR_BONUS_LOTTO_NUMBER);
-        return Integer.parseInt(SCANNER.nextLine());
+        try{
+            return Integer.parseInt(SCANNER.nextLine());
+        } catch (NumberFormatException exception){
+            throw new IllegalArgumentException(MESSAGE_FOR_INVALID_NUMBER);
+        }
     }
     public static void printLottos(Lottos lottos){
         lottos.getLottos()
