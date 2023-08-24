a3a5268d889bee031e78e07af0a788885d6fe3f7
apfhd12gk <apfhd12gk@naver.com>
2023-08-24 22:56:06 +0900

    Lotto클래스 함수 정리 및 getSortedLottoNumbers생성

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,34 +1,44 @@
 package lotto.domain;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     public static final int SINGLE_LOTTO_PRICE = 1000;
     private static final int LOTTO_SIZE = 6;
-    private static final String ERROR_MESSAGE_FOR_INVALID_SIZE_OF_LOTTO_NUMBER ="[ERROR] %d개의 숫자를 골라 주세요.";
-    private static final String ERROR_MESSAGE_FOR_DUPLICATE_LOTTO_NUMBER = "[ERROR] 숫자는 중복될 수 없습니다.";
+    private static final String ERROR_MESSAGE_FOR_INVALID_SIZE_OF_LOTTO_NUMBER = String.format("%d개의 숫자를 골라주세요.", LOTTO_SIZE);
     private final Set<Integer> lottoNumbers;
 
     public Lotto(Set<Integer> lottoNumbers) {
-        validateSize(lottoNumbers);
-        this.lottoNumbers = Collections.unmodifiableSet(lottoNumbers);
+        this.lottoNumbers = lottoNumbers;
     }
 
-    private void validateSize(Set<Integer> numbers) {
+    public static Lotto from(Set<Integer> lottoNumbers){
+        validateSize(lottoNumbers);
+        return new Lotto(lottoNumbers.stream()
+                .collect(Collectors.toUnmodifiableSet()));
+    }
+
+    private static void validateSize(Set<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(ERROR_MESSAGE_FOR_INVALID_SIZE_OF_LOTTO_NUMBER);
         }
     }
 
-    public int getSameNumberCount(Lotto anotherLotto){
-        return (int) lottoNumbers.stream()
-                .filter(anotherLotto::containsLottoNumber).count();
+    public int getSameNumberCount(Lotto otherLotto){
+        List<Integer> intersection = new ArrayList<>(lottoNumbers);
+        intersection.retainAll(otherLotto.lottoNumbers);
+
+        return intersection.size();
     }
     public boolean containsLottoNumber(int lottoNumber){
         return lottoNumbers.contains(lottoNumber);
     }
-    public Set<Integer> getLottoNumbers(){
-        return lottoNumbers;
+    public List<Integer> getSortedLottoNumbers(){
+        return lottoNumbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
